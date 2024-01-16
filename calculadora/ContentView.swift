2ed0680c6ed0356e8768bebb213c1ac6db244027
matheusgates <teteucosta56@gmail.com>
2024-01-16 14:58:35 -0300

    Initial Commit

---
+++ calculadora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calculadora
+//
+//  Created by Matheus Gates on 16/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calculadora/calculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculadoraApp.swift
+//  calculadora
+//
+//  Created by Matheus Gates on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct calculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
